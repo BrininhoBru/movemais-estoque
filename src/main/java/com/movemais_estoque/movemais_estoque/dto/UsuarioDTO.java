fbbe127d479dc1e2113b3db10a1de1034d7d9e37
Bruno Franchini <brunoc.franchini@gmail.com>
2025-11-27 20:49:34 -0300

    🏷️ Add Usuario Entity and UsuarioDTO

---
+++ src/main/java/com/movemais_estoque/movemais_estoque/dto/UsuarioDTO.java
@@ -0,0 +1,9 @@
+package com.movemais_estoque.movemais_estoque.dto;
+
+import lombok.Data;
+
+@Data
+public class UsuarioDTO {
+    private String nome;
+    private String email;
+}
---
+++ src/main/java/com/movemais_estoque/movemais_estoque/model/Usuario.java
@@ -0,0 +1,27 @@
+package com.movemais_estoque.movemais_estoque.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+}
