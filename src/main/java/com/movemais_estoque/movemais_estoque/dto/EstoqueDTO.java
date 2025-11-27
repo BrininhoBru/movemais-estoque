53542bcbf9b7454054800793b09f0f8e53554e9c
Bruno Franchini <brunoc.franchini@gmail.com>
2025-11-27 19:24:28 -0300

    🏷️ Add EstoqueEntity and EstoqueDTO

---
+++ src/main/java/com/movemais_estoque/movemais_estoque/dto/EstoqueDTO.java
@@ -0,0 +1,4 @@
+package com.movemais_estoque.movemais_estoque.dto;
+
+public class EstoqueDTO {
+}
---
+++ src/main/java/com/movemais_estoque/movemais_estoque/model/Estoque.java
@@ -0,0 +1,29 @@
+package com.movemais_estoque.movemais_estoque.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Estoque {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
+
+    @ManyToOne
+    @JoinColumn(name = "deposito_id")
+    private Deposito deposito;
+
+    @Column(nullable = false)
+    private Integer quantidade;
+}
