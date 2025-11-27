f9b4ce8dbea3377d67c1ee4f5fa06cd10173b7b1
Bruno Franchini <brunoc.franchini@gmail.com>
2025-11-27 17:51:11 -0300

    🎉 Begin project

---
+++ src/main/java/com/movemais_estoque/movemais_estoque/MoveMaisEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.movemais_estoque.movemais_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveMaisEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveMaisEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movemais_estoque/movemais_estoque/MoveMaisEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movemais_estoque.movemais_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveMaisEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
