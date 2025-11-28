d1ae12912cc9ce5978d68c58003403f1c68a4545
Bruno Franchini <brunoc.franchini@gmail.com>
2025-11-27 21:07:51 -0300

    ✏️ Fix EstoqueDTO
    Add missing properties

--- src/main/java/com/movemais_estoque/movemais_estoque/dto/EstoqueDTO.java
+++ src/main/java/com/movemais_estoque/movemais_estoque/dto/EstoqueDTO.java
@@ -1,4 +1,10 @@
 package com.movemais_estoque.movemais_estoque.dto;
 
+import lombok.Data;
+
+@Data
 public class EstoqueDTO {
+    private Long produtoId;
+    private Long depositoId;
+    private Integer quantidade;
 }
