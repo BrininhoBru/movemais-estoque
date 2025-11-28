8ba2340bd1321d37ae366fa0bbdc34f49246a4c0
Bruno Franchini <brunoc.franchini@gmail.com>
2025-11-27 21:22:40 -0300

    ♻️ Making Estoque Entity relationships mandatory

--- src/main/java/com/movemais_estoque/movemais_estoque/model/Estoque.java
+++ src/main/java/com/movemais_estoque/movemais_estoque/model/Estoque.java
@@ -14,12 +14,12 @@ public class Estoque {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "produto_id")
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "produto_id", nullable = false)
     private Produto produto;
 
-    @ManyToOne
-    @JoinColumn(name = "deposito_id")
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "deposito_id", nullable = false)
     private Deposito deposito;
 
     @Column(nullable = false)
