9035d5398219e701d6aba53c22107c4e426ebd3f
Bruno Franchini <brunoc.franchini@gmail.com>
2025-11-28 22:50:52 -0300

    🐛 Fix jackson-datatype-jsr310 error

--- src/main/java/com/movemais_estoque/movemais_estoque/response/ApiResponse.java
+++ src/main/java/com/movemais_estoque/movemais_estoque/response/ApiResponse.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 public class ApiResponse<T> {
@@ -11,14 +12,12 @@ public class ApiResponse<T> {
     private final boolean success;
     private final String message;
     private final T data;
-
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private final LocalDateTime timestamp;
+    private final String timestamp;
 
     public ApiResponse(boolean success, String message, T data) {
         this.success = success;
         this.message = message;
         this.data = data;
-        this.timestamp = LocalDateTime.now();
+        this.timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 }
